b163faae6439f881106f9e6fe1ca423720ebb74b
sergi_borras <sergi@getyourguide.com>
2013-11-15 10:49:06 +0100

    added xcode project

---
+++ Thresholds/SRGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SRGAppDelegate.h
+//  Thresholds
+//
+//  Created by Sergi Borras on 11/15/13.
+//  Copyright (c) 2013 Sergi Borras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Thresholds/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thresholds
+//
+//  Created by Sergi Borras on 11/15/13.
+//  Copyright (c) 2013 Sergi Borras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SRGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SRGAppDelegate class]));
+    }
+}
